7652950658887f39574670d52e7a3b53bb8728d2
Shriya Hireholi <hireholis@gmail.com>
2024-02-29 13:37:18 -0800

    Changes for home page. Connection to Login/Signup functionality

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, session, render_template, request, redirect, jsonify
+from flask import Flask, session, render_template, request, redirect, jsonify, url_for
 from flask_session import Session
 from db_connection import db, app
 from sqlalchemy import text
@@ -9,47 +9,48 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
-
-
 @app.route("/")
-def index():
+def check_user_session():
+	is_logged_in = False
 	if session.get("username") is not None:
-		return render_template("home.html", name=session.get("username"))
+		is_logged_in = True
+	return redirect(url_for('home_page', is_active=is_logged_in))
+
+
+@app.route("/temp_home", methods=["GET", "POST"])
+def home_page():
+	is_active = request.args.get('is_active') == "True"
+	query = f"SELECT TOP 10 * FROM books ORDER BY average_rating DESC;"
+	get_top_10_books = db.session.execute(text(query))
+	return render_template("search_result.html", ans=get_top_10_books, is_active=is_active)
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+	session.clear()
+	user_name = request.form.get("name")
+	user_password = request.form.get("pwd")
+	print(user_name)
+	print(user_password)
+	if user_name and user_password:
+		query = f"SELECT user_password FROM users WHERE username LIKE '%{user_name}%';"
+		password = db.session.execute(text(query)).fetchone()
+		if password is not None and user_password == password[0]:
+			session["username"] = user_name
+			return redirect(url_for('home_page', is_active=True))
+		else:
+			return(redirect(url_for('signup_page')))
 	return render_template("login.html")
 
-@app.route("/home", methods=["POST"])
-def home():
-	session.clear()
-	name = request.form.get("name")
-	dbpwd = db.execute("SELECT user_password FROM users WHERE username = :username", {"username": name}).fetchone()
-	if dbpwd != None:
-		pwd = request.form.get("pwd")
-		if pwd == dbpwd[0]:
-			session["username"] = name
-			return render_template("home.html", name=name)
-	else:
-		return redirect("/signup")
-	return render_template("login.html")
-
-@app.route("/login", methods=["POST"])
-def login():
-	session.clear()
-	name = request.form.get("name")
-	pwd = request.form.get("pwd")
-	db.session.execute(text("INSERT INTO users (username, user_password) VALUES (:username, :password);"),{"username": name, "password": pwd})
-	
-	db.session.commit()
-	
-	session["username"] = name
-	return render_template("home.html", name=name)
-
-@app.route("/signup")
-def signup():
-	if session.get("username") is not None:
-		return render_template("home.html", name=session.get("username"))
+@app.route("/signup", methods=["GET", "POST"])
+def signup_page():
+	user_name = request.form.get("name")
+	user_password = request.form.get("pwd")
+	if user_name and user_password:
+		query = text("INSERT INTO users (username, user_password) VALUES (:username, :user_password);")
+		db.session.execute(query,{"username": user_name, "user_password": user_password})
+		db.session.commit()
+		session["username"] = user_name
+		return redirect(url_for('home_page', is_active=True))
 	return render_template("signup.html")
 
 @app.route("/find", methods=["POST"])
@@ -82,10 +83,10 @@ def queryTwo(n1, v1, n2, v2):
 def details(isbn):
 	query = f"SELECT * FROM books WHERE isbn={isbn}"
 	ans =db.session.execute(text(query)).fetchone()
-	response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": 'JkwcGThDCN97xqvW7Stg', "isbns": isbn})
-	sna = response.json()
-	if response.status_code != 200:
-		raise Exception("ERROR: API request unsuccessful.")
+	# response = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": 'JkwcGThDCN97xqvW7Stg', "isbns": isbn})
+	# sna = response.json()
+	# if response.status_code != 200:
+	# 	raise Exception("ERROR: API request unsuccessful.")
 	reviews_query = f"SELECT rating, review, username FROM reviews WHERE isbn = {isbn}"
 	reviews = db.session.execute(text(reviews_query)).fetchall()
 	return render_template("details.html", res=ans, avg=sna['books'][0]['average_rating'], rating=sna['books'][0]['average_rating'], total=sna['books'][0]['ratings_count'], reviews=reviews)
